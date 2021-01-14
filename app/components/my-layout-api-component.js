8e59770ccdc9100e845dc02a8ca027f8feaa2b28
Pat O'Callaghan <pat@intercom.io>
2021-01-14 15:33:55 +0000

    Add layout + layoutName spike

---
+++ app/components/my-layout-api-component.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+import MyReusableTemplate from 'ember-realworld/templates/components/my-reusable-template';
+
+export default Component.extend({
+  layout: MyReusableTemplate,
+  // layoutName: 'components/my-reusable-template',
+});
