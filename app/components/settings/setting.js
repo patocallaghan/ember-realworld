21689d54882f5688a1747496e19c88823701d782
Pat O'Callaghan <pat@intercom.io>
2021-01-18 11:27:56 +0000

    Make settings import async

--- app/components/settings/setting.js
+++ app/components/settings/setting.js
@@ -1,20 +1,28 @@
 import Component from '@glimmer/component';
 import { ensureSafeComponent } from '@embroider/util';
-import { importSync } from '@embroider/macros';
+import { task } from 'ember-concurrency-decorators';
 
 const APP_PACKAGES_WITH_SETTINGS = ['a', 'b', 'c', 'd'];
 
 export default class Settings extends Component {
+  constructor() {
+    super(...arguments);
+    this.loadSettingsComponentIfAny.perform();
+  }
+
+  @task
+  *loadSettingsComponentIfAny() {
+    if (this.hasSettingsComponent) {
+      let module = yield import(`../settings/${this.args.setting.identifier}/main`);
+      this.settingsComponent = ensureSafeComponent(module.default, this);
+    }
+  }
+
   get hasSettingsComponent() {
     return APP_PACKAGES_WITH_SETTINGS.includes(this.args.setting.identifier);
   }
 
   get settingsComponentPath() {
     return `settings/${this.args.setting.identifier}/main`;
-  }
-
-  get settingsComponent() {
-    let module = importSync(`../settings/${this.args.setting.identifier}/main`);
-    return ensureSafeComponent(module.default, this);
   }
 }
